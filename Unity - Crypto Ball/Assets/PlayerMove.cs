fbf0a4303fd0425473a22efe8eb16c641da9ccb1
Victor Lalo <vlalo0624@gmail.com>
2021-02-10 23:17:45 -0800

    Character sync

--- Unity - Crypto Ball/Assets/PlayerMove.cs
+++ Unity - Crypto Ball/Assets/PlayerMove.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerMove : MonoBehaviour, IPunObservable
+public class PlayerMove : MonoBehaviour//, IPunObservable
 {
     float speed = 10f;
     Rigidbody rb;
 
     PhotonView photonView;
-    [SerializeField] MonoBehaviour[] scriptsToIgnore;
+    //[SerializeField] MonoBehaviour[] scriptsToIgnore;
 
     void Start()
     {
@@ -17,34 +17,29 @@ public class PlayerMove : MonoBehaviour, IPunObservable
         GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(Random.Range(0f, 1f), 1f, 1f);
 
         photonView = GetComponent<PhotonView>();
-        if (!photonView.IsMine)
-        {
-            foreach(var script in scriptsToIgnore)
-            {
-                script.enabled = false;
-            }
-        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horiz = Input.GetAxisRaw("Horizontal");
-        float vert = Input.GetAxisRaw("Vertical");
-
-        rb.MovePosition(rb.position + new Vector3(horiz, vert) * Time.fixedDeltaTime * speed );
-
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
+        if (photonView.IsMine)
         {
-            stream.SendNext(rb.position);
-        }
-        else
-        {
-            rb.position = (Vector3)stream.ReceiveNext();
+            float horiz = Input.GetAxisRaw("Horizontal");
+            float vert = Input.GetAxisRaw("Vertical");
+
+            rb.MovePosition(rb.position + new Vector3(horiz, vert) * Time.fixedDeltaTime * speed);
         }
     }
+
+    //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //{
+    //    if (stream.IsWriting)
+    //    {
+    //        stream.SendNext(rb.position);
+    //    }
+    //    else
+    //    {
+    //        rb.position = (Vector3)stream.ReceiveNext();
+    //    }
+    //}
 }
