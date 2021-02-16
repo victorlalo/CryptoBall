2aea689b599db2f82a22600c3ea0416f2c0fc718
Victor Lalo <vlalo0624@gmail.com>
2021-02-15 19:21:00 -0800

    Character Generation

---
+++ Unity - Crypto Ball/Assets/Scripts/Generation/TrophyRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrophyRotate : MonoBehaviour
+{
+    public float rotSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
+    }
+}
